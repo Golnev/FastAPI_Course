8c7c1662e0e6f8df3685e007f65b714e2950b378
Eugene Golnev <eugenegolnev1993@gmail.com>
2023-03-16 19:20:08 +0300

    get in db and HTTPExeption

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
 
 from blog import models
@@ -20,7 +20,7 @@ def get_db():
         db.close()
 
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(
         title=request.title,
@@ -30,3 +30,19 @@ def create(request: Blog, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get('/blog')
+def get_all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}')
+def get_single(pk: int, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == pk).first()
+    if not blog:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'details': f'Blog with the ID {pk} is not available'}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with the ID {pk} is not available')
+    return blog
